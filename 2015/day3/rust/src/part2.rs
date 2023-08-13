d1e9f32fa6a08f2e9bc90ec6781d45fbd366e7aa
GodfatherRT <maks12ate@gmail.com>
2023-08-13 16:04:29 +0300

    start day3 2015 rust

---
+++ 2015/day3/rust/src/part2.rs
@@ -0,0 +1,8 @@
+pub fn part2() -> String {
+    let file_path = "input.txt";
+    let string = std::fs::read_to_string(file_path).expect("Failed to read input file");
+
+    let mut res = 0;
+
+    format!("Answer for part1: {}", res)
+}
