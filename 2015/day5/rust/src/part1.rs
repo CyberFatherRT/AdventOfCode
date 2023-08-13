706d59aecf22414e1d206bcbc5a1717881c110df
GodfatherRT <maks12ate@gmail.com>
2023-08-13 18:44:01 +0300

    solve day5 2015 rust

--- 2015/day5/rust/src/part1.rs
+++ 2015/day5/rust/src/part1.rs
@@ -4,8 +4,41 @@ pub fn part1() -> String {
     let file_path = "../../inputs/day5.txt";
     let string = fs::read_to_string(file_path).unwrap();
 
-    let mut res = 0;
+    let mut ans = 0;
 
+    for line in string.lines() {
+        let mut res = 0;
+        let mut prev: char = ' ';
+        let (mut flag1, mut flag2, mut flag3) = (true, true, false);
+        let mut vowels = 0;
 
-    format!("Answer for part1: {}", res)
+        for c in line.chars() {
+            if "aeiou".contains(c) {
+                vowels += 1;
+            }
+            if c == prev && flag2 {
+                res += 1;
+                flag2 = false;
+            }
+            if ["ab", "cd", "pq", "xy"].contains(&&*format!("{}{}", prev, c)) {
+                flag3 = true;
+                break;
+            }
+
+            if vowels == 3 && flag1 {
+                res += 1;
+                flag1 = false;
+            }
+
+            prev = c;
+        }
+        if flag3 {
+            continue;
+        }
+        if res == 2 {
+            ans += 1;
+        }
+    }
+
+    format!("Answer for part1: {}", ans)
 }
--- 2015/day5/rust/src/part2.rs
+++ 2015/day5/rust/src/part2.rs
@@ -1,11 +1,37 @@
-use std::fs;
+use std::process::{Command, Stdio};
 
 pub fn part2() -> String {
     let file_path = "../../inputs/day5.txt";
-    let string = fs::read_to_string(file_path).unwrap();
 
-    let mut res = 0;
+    let cat = Command::new("cat")
+        .arg(file_path)
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
 
+    let grep1 = Command::new("grep")
+        .arg(r"\(..\).*\1")
+        .stdin(Stdio::from(cat.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
 
-    format!("Answer for part2: {}", res)
+    let grep2 = Command::new("grep")
+        .arg(r"\(.\).\1")
+        .stdin(Stdio::from(grep1.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let wc = Command::new("wc")
+        .arg("-l")
+        .stdin(Stdio::from(grep2.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let output = wc.wait_with_output().unwrap();
+    let ans = String::from_utf8(output.stdout).unwrap();
+
+    format!("Answer for part2: {}", ans.trim())
 }
